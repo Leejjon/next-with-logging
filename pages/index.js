287e7c64238e8aa824162160e611cf7bc65d123b
Leon Liefting <leon.liefting@persgroep.net>
2021-09-17 15:38:10 +0200

    Let's see if we still log.

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
-import Amplify, {AWSCloudWatchProvider, Logger} from 'aws-amplify';
+import Amplify, {Logger} from 'aws-amplify';
 import config from "../aws-exports.js";
 
 Amplify.configure({
-    Logging: {
-        logGroupName: 'plugin-1',
-        logStreamName: '1-plugin'
-    },
     ...config,
-    // ssr: true
+    ssr: true
 });
 
 const browserLogger = new Logger('Log to browser');
-const ssLogger = new Logger('Log to cloudwatch');
 
-Amplify.register(ssLogger);
-ssLogger.addPluggable(new AWSCloudWatchProvider());
+export async function getServerSideProps({params}) {
+    console.log("Still doing serverside rendering...");
+    return {props: {ssrData: {name: "Some SSR data", timestamp: (new Date()).toUTCString()}}};
+}
 
-const App = () => {
+const App = ({ssrData}) => {
     return (
         <div onClick={() => {
             browserLogger.warn('Clicking??');
             ssLogger.warn('Clicking??');
-        }}>Boe</div>
+        }}>{ssrData.timestamp}</div>
     )
 }
 
