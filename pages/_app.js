7a4b367ef8414937bc9deb69ab0199172ce21b19
Leon Liefting <leon.liefting@persgroep.net>
2021-09-17 15:24:50 +0200

    Let's try without serverside rendering.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,4 @@
 import '../styles/globals.css';
-import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
-
-Amplify.configure({
-  ...awsconfig,
-  ssr: true}
-);
 
 function MyApp({Component, pageProps}) {
     return (
