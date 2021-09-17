cf094a47d58a09530115aa14b1c7da1cb5f78dcb
Leon Liefting <leon.liefting@persgroep.net>
2021-09-17 13:54:34 +0200

    Push the SSR function.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+Amplify.configure({
+  ...awsconfig,
+  ssr: true}
+);
+
+function MyApp({Component, pageProps}) {
+    return (
+        <Component {...pageProps} />
+    );
 }
 
-export default MyApp
+export default MyApp;
