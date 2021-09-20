aee088da4409a5392b92905c7126dc8fe45f17d5
Leon Liefting <leon.liefting@persgroep.net>
2021-09-20 09:27:11 +0200

    The Amplify configuration wasn't needed for just logging.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import Amplify from "aws-amplify";
-import config from "../../aws-exports.js";
-
-// Amplify SSR configuration needs to be done within each API route
-// Amplify.configure({ ...config, ssr: true });
 
 export default function handler(req, res) {
   console.log('Running the API route.');
